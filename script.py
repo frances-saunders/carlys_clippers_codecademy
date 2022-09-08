326eb6974368c7c562297333c0476684120dfa89
Frances Saunders <92679675+frances-saunders@users.noreply.github.com>
2022-09-07 21:13:38 -0400

    Update script.py

--- script.py
+++ script.py
@@ -1,3 +1,5 @@
+#This project fulfills the reqs for "Carly's Clippers" in Codecademy
+
 hairstyles = ["bouffant", "pixie", "dreadlocks", "crew", "bowl", "bob", "mohawk", "flattop"]
 
 prices = [30, 25, 40, 20, 20, 35, 50, 35]
